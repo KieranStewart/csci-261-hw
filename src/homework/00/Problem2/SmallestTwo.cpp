e5be64eb507aa376260a9858931efcc4b77cb12a
Kieran Stewart <kieranmstewart@gmail.com>
2025-09-05 21:34:31 -0400

    Commented SmallestTwo into oblivion

--- src/homework/00/Problem2/SmallestTwo.cpp
+++ src/homework/00/Problem2/SmallestTwo.cpp
@@ -1,9 +1,10 @@
-// For a given n list all prime numbers $\leq n$ in increasing order
+// Finds smallest two elements in an array
 
 #include <iostream>
 
-const int MAX_SIZE = 1000;
+const int MAX_SIZE = 10000; // Maximum size for the input array
 
+// Lets me return two elements without having to pass an array
 struct Pair {
     int smallest;
     int second;
@@ -11,25 +12,29 @@ struct Pair {
 
 struct Pair smallestTwo(int* array, int length) {
     int smallest = array[0];
+    // Find the smallest value
     for (int i = 1; i < length; i ++) {
         if (array[i] < smallest) smallest = array[i];
     }
     int second;
     bool secondInit = true; // Represents whether second still needs to get a value
     for (int i = 0; i < length; i ++) {
+        // if the item at i is not the smallest, and either the second smalles doesn't have a
+        //  value yet or this one is smaller then the value it currently has
         if ((secondInit || (array[i] < second)) && (array[i] != smallest)) {
-            secondInit = false;
+            secondInit = false; // Second now has a value
             second = array[i];
         }
     }
+    // Add the two results to a pair and returns them
     Pair out = {smallest, second};
     return out;
 }
 
 int main () {
-    int input[10000];
-    int inputLen;
-    std::cin >> inputLen;
+    int input[MAX_SIZE];
+    int inputLen; // The size for input
+    std::cin >> inputLen; // Assigning the size
     for (int i = 0; i < inputLen; i ++) {
         std::cin >> input[i];
     }
